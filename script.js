c3173d6a44361f87596ffb3bef80903338eac4a4
Yalon Gordon <yalon@Yalons-MBP.home>
2018-11-30 12:11:42 -0500

    toggle group checkboxes active

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
-
+var toggler = 0;
 $(document).ready(function(){
 
     //idea #1: global variables for the id's of each set of checkboxes+fieldset
     var targetCheckBox = ":checkbox";
-    
-
-
+    var targetField = "#xy";
 
     $(":checkbox").enhancedCheckBox();
 
     $(":checkbox").enhancedCheckBox("checkEm");
 
-    $("fieldset").click(function(){
+    $("fieldset").click(function(event){
+            event.stopPropagation();
+            event.preventDefault();
+            event.stopImmediatePropagation();
+       
             //idea #2 parse id of clicked box, match to distinct ids of each set
             //var fields = ;
-            var children = $(this).children(":checkbox");
-           
-           /* if(children.prop("checked")){
-            children.prop("checked", false)
-            
-            $( children ).checkboxradio( "refresh" );
-                     }
-            else{*/
-            children.prop("checked", true);//}
-            //get children
-
-            //activate
+            var children = $(this).children(targetCheckBox);
+            if(toggler==0){
+             children.prop("checked", true);console.log(toggler++ + " checked"); }
+             else{
+             children.prop("checked", false);  console.log(toggler-- + " unchecked");}
+        
     });
 
 
@@ -33,41 +29,20 @@ $(document).ready(function(){
    
 })
 
-document.getElementById("hi").innerHTML = "bye";
+
 
 
 $.widget("custom.enhancedCheckBox", $.ui.checkboxradio, {
     enabled: true,
+    icon: false,
     checkEm: function(){
-       // $(this).element.sibilings(":checkbox").prop("checked", "checked");
-       // testd($(this).element.prop("checked"));
+      
         console.log()
     }
 
-   /* _create: function(){
-        this.element.addClass("checkbox");
-    };*/
-
-    
-    /*this._on(this.parent,{
-        click: function(e){
-            element.prop( "checked", !this.element.is( ":checked" ) );
-                    this.refresh();
-
-            console.log("here");
-        }
-    });*/
-  
-
-    
-
 
 });
-//$("#checkbox-1").prop("checked", true);
 
-function testd(x){
-    console.log(x);
-}
 
     
 
@@ -96,21 +71,7 @@ function testd(x){
 
 
 
- /*
-    var test = document.getElementById("xy"); // need to make general
-    var elem = this;
-    $("fieldset").click(function(){
-        console.log("hello");
-       
-        $("checkbox-1").prop('checked', true);
-
-           
-                var c = this.children.checked;
-                $(':checkbox').prop('checked',c);
-              
-                  
-    });
-*/
+ 
   
     
 
